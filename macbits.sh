b73bab0a4c1bc3c78970ecec23a4073c181cbae0
karalan <karajlangford@gmail.com>
2015-10-06 19:20:05 +0100

    Gestures and Reset
    Added Gestures to Doc Icons and Reset Code

--- macbits.sh
+++ macbits.sh
@@ -1,1 +1,4 @@
 defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
+defaults write com.apple.dock scroll-to-open -bool TRUE;
+--use this to reset everything--
+defaults delete com.apple.dock;
